748a16f1d5e15b54ebb000e5f731c8270cc3bbe6
tjdlwns33 <tjdlwns33@gmail.com>
2025-04-02 00:15:47 +0900

    기본과제 작업

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,42 @@
-export function setupEventListeners(root) {}
+const eventMap = new Map();
+const rootMap = new Map();
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  if (!rootMap.has(root)) {
+    rootMap.set(root, new Set());
+  }
 
-export function removeEvent(element, eventType, handler) {}
+  const rootEventTypeList = rootMap.get(root);
+
+  for (const [eventType, elementMap] of eventMap) {
+    if (rootEventTypeList.has(eventType)) return;
+
+    root.addEventListener(eventType, (event) => {
+      for (const [element, handler] of elementMap) {
+        if (element === event.target || element.contains(event.target)) {
+          handler.call(element, event);
+        }
+      }
+    });
+
+    rootEventTypeList.add(eventType);
+  }
+}
+
+export function addEvent(element, eventType, handler) {
+  if (!eventMap.has(eventType)) {
+    eventMap.set(eventType, new Map());
+  }
+
+  const elementMap = eventMap.get(eventType);
+  elementMap.set(element, handler);
+}
+
+export function removeEvent(element, eventType, handler) {
+  const elementMap = eventMap.get(eventType);
+  if (!elementMap) return;
+
+  if (elementMap.get(element) === handler) {
+    elementMap.delete(element);
+  }
+}
